2f42f808cdcec2ccc9208584da062cd7da9804de
leonokida <le0n0kida@gmail.com>
2021-09-30 13:20:23 -0300

    [add] - Tratamento da entrada

--- ep01.c
+++ ep01.c
@@ -4,7 +4,22 @@
 #include <math.h>
 #include <matheval.h>
 
+#define MAX 50
+
 int main(int argc, char *argv[]) {
-    printf("%s", argv[1]);
+    /* Atribuições e declarações */
+    double x0, epsilon;
+    int max_iter;
+
+    x0 = atof(argv[2]);
+    epsilon = atof(argv[3]);
+    max_iter = atoi(argv[4]);
+
+    /* Obtendo função */
+    void *f;
+    f = evaluator_create(argv[1]);
+
+    /* Tratando função */
+
     return 0;
 }
