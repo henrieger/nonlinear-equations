006e30c61a551cc41b6179ef56ef9420d1242bf4
Henrique Rieger <henriquerieger2001@gmail.com>
2021-10-04 18:04:02 -0300

    Adiciona impressão e cálculo de erros

--- funcoes.c
+++ funcoes.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <matheval.h>
+#include <string.h>
 
 double newtonRaphson(void *f, double * x0Newton) {
     void *f_derivada;
@@ -23,6 +24,14 @@ double secante(void *f, double * x0Secante, double * x1Secante) {
     funcao0 = evaluator_evaluate_x(f, *x0Secante);
 
     return *x1Secante - ((funcao1 * (*x1Secante - *x0Secante))/(funcao1 - funcao0));
+}
+
+/* Transforma um número double em int */
+long long doubleToLongLong(double d)
+{
+    long long l;
+    memcpy(&l, &d, sizeof(double));
+    return l;
 }
 
 void iteracao(void *f, double x0, double epsilon, int max_iter) {
@@ -51,8 +60,12 @@ void iteracao(void *f, double x0, double epsilon, int max_iter) {
 
         /* Calcular newton_crit e secante_crit */
         /* Calcular EA e ER */
+        ea = fabs(secante_x - newton_x);
+        er = ea/secante_x;
+        ulps = abs(doubleToLongLong(secante_x) - doubleToLongLong(newton_x));
 
         /* Imprimir */
+        printf("%d,%1.16e,%1.16e,%1.16e,%1.16e,%d\n", i, newton_x, secante_x, ea, er, ulps);
 
         /* Atribuindo valores para próximas iterações */
         x0Newton = newton_x;
@@ -60,6 +73,8 @@ void iteracao(void *f, double x0, double epsilon, int max_iter) {
             x0Secante = x1Secante;
             x1Secante = secante_x;
         }
+        
+        i++;
 
     } while ((i <= max_iter));
 }
